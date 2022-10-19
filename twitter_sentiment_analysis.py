7886aa95381462c4cc8853db9b31ddfab65293e1
gonzalez <66866763+gonzalezYin@users.noreply.github.com>
2022-10-19 10:56:19 -0400

    Add files via upload

---
+++ twitter_sentiment_analysis.py
@@ -0,0 +1,85 @@
+# encoding: utf-8
+from textblob import TextBlob
+import tweepy
+import botometer
+import sys
+
+# request API
+consumer_key = "XwK3IS0ldbT4FZ5fYLbZ3wDnU"
+consumer_secret = "ytl8OhWaR1xGyO43JC0nfHkOD3e6SjghgKq7t5FVebYaee1zaM"
+access_token = "1579831536119156741-4CF8Y5ehj9kmINXnLRcUlAnRTXsGAN"
+access_token_secret = "lnLf5QuTKqZp7PktjzRHlpOqdYTdQsiRO9VVT4QZ80Vt6"
+rapidapi_key = "5fbf5733d2msh9e24044ca46daccp123a06jsn511d4dead8aa"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+twitter_app_auth = {
+    'consumer_key': consumer_key,
+    'consumer_secret': consumer_secret,
+    'access_token': access_token,
+    'access_token_secret': access_token_secret,
+}
+
+
+def sentiment_analysis(keyword, num):
+    # keyword = 'League of Legends', 'LoL'
+    # retrieve tweets
+    tweets = tweepy.Cursor(api.search, q=keyword, lang="en").items(num)
+
+    # retrieve text in every tweet and remove repeat
+    text_set = set()
+    screen_name_list = []
+    id_list = []
+    for tweet in tweets:
+        text_set.add(tweet.text)
+        screen_name_list.append(tweet.user.screen_name)
+        id_list.append(tweet.user.id)
+        # with open("example.txt", "a+") as f:
+        #     f.write(str(tweet))
+
+    blt = botometer.BotometerLite(rapidapi_key=rapidapi_key, **twitter_app_auth)
+    print(screen_name_list)
+    print(id_list)
+    blt_scores_name = blt.check_accounts_from_screen_names(screen_name_list)
+    blt_scores_id = blt.check_accounts_from_user_ids(id_list)
+    print(blt_scores_name)
+    print(blt_scores_id)
+
+    # sentiment analysis with polarity and subjectivity
+    polarity_score = []
+    # subjectivity_score = []
+    for text in text_set:
+        analysis = TextBlob(text)
+        # print(text)
+        polarity_score.append(analysis.sentiment.polarity)
+        # subjectivity_score.append(analysis.sentiment.subjectivity)
+
+    # compute the sentiment score
+    sum = 0
+    length = len(polarity_score)
+    count = 0
+    for sentence in range(length):
+        sum += polarity_score[sentence]
+        if polarity_score[sentence] == 0:
+            count = count + 1
+        print(f"The polarity score of sentence {sentence} is {polarity_score[sentence]}")
+    average = sum / (length - count)
+
+    # output
+    print(f"The total sentiment score of the {keyword}is {sum}")
+    print(f"The average sentiment score of the {keyword}is {average}")
+
+    # polarity judgement
+    if average > 0:
+        print("nice product!")
+    else:
+        print("not recommended product!")
+    return
+
+
+
+
+
